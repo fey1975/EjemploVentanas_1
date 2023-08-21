892a5ed87a1554302cadf4dc2c900ce6a4699655
fey1975 <fabian.yapura@gmail.com>
2023-08-20 22:43:24 -0300

    Initial commit

---
+++ src/ejemploventanas_1/EjemploVentanas_1.java
@@ -0,0 +1,15 @@
+/*
+ Ejemplo en clase para armar una interfaz gráfica en clases - LAB 1 - Comisión 1
+ */
+package ejemploventanas_1;
+
+/**
+ * @Fabián
+ */
+public class EjemploVentanas_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
